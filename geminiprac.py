6b022f20978ba03b955dd6c2b05eef06f3c02b46
Ashley Ashiku <ashleyashiku@ashleys-MacBook-Pro.local>
2025-03-29 12:00:22 -0500

    trying again

--- geminiprac.py
+++ geminiprac.py
@@ -1,1 +1,8 @@
 from google import genai
+
+client = genai.Client(api_key="AIzaSyAGYW8CZPOtnEtYP_DsC9x35xhSBOLB4ew")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
